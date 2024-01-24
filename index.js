b274fe48f2f1a8c880e48eea50b409048236a161
Ronald Britos <rmbgkpo@ensign.edu>
2024-01-24 09:33:29 -0700

    Building connection of the backend with the database

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const express = require('express');// express makes APIs - connect frotend to database
+const express = require("express"); // express makes APIs - connect frotend to database
+const Redis = require("redis"); //redis is a database, import the Redis class from the redis library
+
+//import redis from 'redis';//import redis library
+
+const redisClient = Redis.createClient({
+  url: `redis://localhost:6379`, //connect to redis on port 6379
+}); //create a redis client
 const app = express(); // create an express application
-app.listen(3000); //listen for web requests form the frontend and don't stop () => console.log('listening at 3000')); // listen for requests on port 3000
+const port = 3000; // port to run the server on
+app.listen(port, () => {
+  redisClient.connect(); //connect to redis
+  console.log(`API is listening on port: ${port}`); //template literal
+}); //listen for web requests form the frontend and don't stop () => console.log('listening at 3000')); // listen for requests on port 3000
+
 //make a list of boxes
-const boxes = [
-    { name: 'Box1', boxid: 1 },
-    { name: 'Box2', boxid: 2 },
-    { name: 'Box3', boxid: 3 },
-    { name: 'Box4', boxid: 4 }
-];
+// const boxes = [
+//   { name: "Box1", boxid: 1 },
+//   { name: "Box2", boxid: 2 },
+//   { name: "Box3", boxid: 3 },
+//   { name: "Box4", boxid: 4 },
+// ]; //hardcoded boxes-not in the database.
 //1-URL
 //2-Callback function
 //3-Response
 //req=request
 //res=response
 
-app.get('/boxes', (req,res)=>{
-    res.send(JSON.stringify(boxes));//convert boxes to a string and send it to the user
-});//return boxes to the user
+app.get("/boxes", async (req, res) => {
+  let boxes = await redisClient.json.get("boxes", { path: "$" }); //get boxes from redis
+  res.send(JSON.stringify(boxes)); //convert boxes to a string and send it to the user
+}); //return boxes to the user
